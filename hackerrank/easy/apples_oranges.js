e13420d77e8d480de23ca5a6b900806f7484934c
Prescott Breeden <prescott.breeden@gmail.com>
2018-06-16 16:58:48 -0700

    sync

--- hackerrank/easy/apples_oranges.js
+++ hackerrank/easy/apples_oranges.js
@@ -1,6 +1,17 @@
 // apples and oranges
+/*
+The first line contains two space-separated integers denoting the respective values of s and t. 
+The second line contains two space-separated integers denoting the respective values of a and b. 
+The third line contains two space-separated integers denoting the respective values of m and n. 
+The fourth line contains m space-separated integers denoting the respective distances that each apple falls from point a. 
+The fifth line contains n space-separated integers denoting the respective distances that each orange falls from point b.
+*/
+a + d = s;
+b - d = t;
+t - s = house.width;
 
-function applesOranges() {
+
+function applesOranges(s, t, a, b, apples, oranges) {
 
 
 
