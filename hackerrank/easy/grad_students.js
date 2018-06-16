86e92fc071c7ae7e6ec0e8f57958c683b5d611d8
prescottbreeden <prescott.breeden@gmail.com>
2018-06-15 23:29:00 -0700

    changing comps
    updating recent workfile

---
+++ hackerrank/easy/grad_students.js
@@ -0,0 +1,16 @@
+// every student receives a grade in the inclusive range from 0 to 100
+// any grade less than 40 is a failing grade
+
+// if the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5
+
+// if the value of grade is less than 38, no rounding occurs as the result will still be a failing grade
+
+// take an integer array
+// sample input: [73, 67, 38, 33]
+// sample output: [75, 67, 40, 33]
+
+function roundgrades(arr) {
+
+
+
+}
