b64c1f6ba95bcf1131d389591b3679cc7897d109
prescottbreeden <prescott.breeden@gmail.com>
2018-09-06 17:20:32 -0700

    find prime factors python

---
+++ python/list_factors.py
@@ -0,0 +1,26 @@
+def list_factors2(num):
+    res = ([int(num/i) for i in range(2, int(num)) if (num/i).is_integer()])
+    return res[::-1]
+
+
+def list_factors(num, left_side=None, right_side=None):
+    if left_side is None:
+        left_side = []
+    factors = []
+    for i in range(2, int(num)):
+        if (num/i).is_integer():
+            factors.append(i)
+    factors = factors[::-1]
+    if factors:
+        left_side.append(int(num/factors[0]))
+        right_side = factors[0]
+        list_factors(factors[0], left_side, right_side)
+    else:
+        left_side.append(right_side)
+        print(left_side)
+
+
+list_factors(117)
+list_factors(200)
+list_factors(6)
+list_factors(666)
